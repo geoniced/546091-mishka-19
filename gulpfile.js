591380204f346659d1b3a6b7445154aa87da443c
Ivan Kashin <barbos1701@gmail.com>
2020-05-12 19:37:31 +0300

    Добавляет стилизацию и логику модального окна, правит тени, переносит normalize в scss и добавляет задачу с js

--- gulpfile.js
+++ gulpfile.js
@@ -104,6 +104,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.scss", gulp.series("css"));
+  gulp.watch("source/js/*", gulp.series("build", "refresh"));
   gulp.watch(["source/img/icon-*.svg", "source/img/logo-*.svg"], gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,7 @@
 var menuButton = document.querySelector('.main-nav__menu');
 var mainNav = document.querySelector('.main-nav');
+var orderButtons = document.querySelectorAll('.js-order');
+var orderModal = document.querySelector('.modal-order');
 
 mainNav.classList.remove('main-nav--no-js');
 
@@ -12,3 +14,44 @@ menuButton.addEventListener('click', function(evt) {
     mainNav.classList.add('main-nav--opened');
   }
 });
+
+for (var i = 0; i < orderButtons.length; i++) {
+  var orderButton = orderButtons[i];
+
+  orderButton.addEventListener('click', orderButtonHandler);
+}
+
+function modalCloseHandler(evt) {
+  var blocklayer = document.querySelector('.blocklayer');
+  orderModal.classList.remove('modal--open');
+  orderModal.classList.add('modal--closed');
+
+  document.body.removeChild(blocklayer);
+};
+
+function orderButtonHandler(evt) {
+  evt.preventDefault();
+
+  var addToCartButton = orderModal.querySelector('.modal-order__add');
+
+  var blocklayer = document.createElement('div');
+  blocklayer.classList.add('blocklayer');
+
+  blocklayer.addEventListener('click', modalCloseHandler);
+  window.addEventListener('keydown', keydownHandler);
+  addToCartButton.addEventListener('click', modalCloseHandler);
+
+  document.body.appendChild(blocklayer);
+
+  orderModal.classList.remove('modal--closed');
+  orderModal.classList.add('modal--open');
+};
+
+function keydownHandler(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    modalCloseHandler();
+
+    window.removeEventListener('keydown', keydownHandler);
+  }
+}
